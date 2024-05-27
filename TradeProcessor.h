d2b923fe8a8bc8defbdd80d85c339b6fefde32ae
Shreyasshekar <shreyasshekar21@gmail.com>
2024-05-27 10:42:52 +0530

    Add files via upload
    Updated files with fix

--- TradeProcessor.h
+++ TradeProcessor.h
@@ -2,7 +2,8 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-
+#include <string>
+ 
 struct TR
 {
     std::string SC;//SourceCurrency
@@ -10,67 +11,19 @@ struct TR
     float Lots;
     double Price;
 };
-
+ 
 class Processor
 {
 public:
     void Process(std::istream& stream);
-    
+   
 private:
     static const float LotSize;
-    std::vector<std::string> SplitString(const std::string& str, char delimiter)
-    {
-        std::vector<std::string> tokens;
-        std::string token;
-        std::istringstream tokenStream(str);
-        while (std::getline(tokenStream, token, delimiter))
-        {
-            tokens.push_back(token);
-        }
-        return tokens;
-    }
-
-    bool intGetFromString(const std::string& str, int& value)
-    {
-        try
-        {
-            value = std::stoi(str);
-            return true;
-        }
-        catch (const std::exception&)
-        {
-            return false;
-        }
-    }
-
-    bool toDouble(const std::string& str, double& value)
-    {
-        try
-        {
-            value = std::stod(str);
-            return true;
-        }
-        catch (const std::exception&)
-        {
-            return false;
-        }
-    }
+    std::vector<std::string> SplitString(const std::string& str, char delimiter);
+    bool CheckIntegerValidity(const std::string& str, int& value);
+    bool CheckDoubleValidity(const std::string& str, double& value);
+    bool ProcessLine(const std::string& line, TR& trade, int lineNumber);
+    std::string GenerateXML(const std::vector<TR>& trades);
 };
 const float Processor::LotSize = 100000;
-
-// int main()
-// {
-//     TradeProcessor processor;
-//     std::ifstream file("trades.txt");
-//     if (file.is_open())
-//     {
-//         processor.ProcessTrades(file);
-//         file.close();
-//     }
-//     else
-//     {
-//         std::cout << "Failed to open file" << std::endl;
-//     }
-//     return 0;
-// }
-
+ 
